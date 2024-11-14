0c81863b830205782ccc2fe9e3ba6e871974abfb
Sagar R Anghan <sagar@targeticon.com>
2024-11-14 14:05:52 +0530

    Update : intl lib

--- example/ios/Flutter/flutter_export_environment.sh
+++ example/ios/Flutter/flutter_export_environment.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
 export "FLUTTER_ROOT=/Users/stackappinfotech/Documents/flutter"
-export "FLUTTER_APPLICATION_PATH=/Users/stackappinfotech/Desktop/sagar/easy_date_timeline"
+export "FLUTTER_APPLICATION_PATH=/Users/stackappinfotech/Desktop/sagar/easy_date_timeline/example"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_TARGET=lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
-export "FLUTTER_BUILD_NAME=0.1.1"
-export "FLUTTER_BUILD_NUMBER=0.1.1"
+export "FLUTTER_BUILD_NAME=1.0.0"
+export "FLUTTER_BUILD_NUMBER=1"
 export "DART_OBFUSCATION=false"
 export "TRACK_WIDGET_CREATION=true"
 export "TREE_SHAKE_ICONS=false"
